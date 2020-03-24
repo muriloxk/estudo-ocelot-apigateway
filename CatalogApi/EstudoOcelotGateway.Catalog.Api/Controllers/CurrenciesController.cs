624f6c64ab65d6112890fdcbd27e3016c208758c
Murilo Sanches <murilando@gmail.com>
2020-03-24 18:02:04 -0300

    Re routes and aggregates como primeiro exemplo

---
+++ CatalogApi/EstudoOcelotGateway.Catalog.Api/Controllers/CurrenciesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstudoOcelotGateway.Catalog.Api.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr", "jpy" };
+        }
+    }
+}
---
+++ OrdersApi/EstudosOcelotGateway.Orders.Api/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EstudosOcelotGateway.Orders.Api.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public IActionResult GetOrders()
+        {
+            var o1 = new Order("ID1", 200);
+            var o2 = new Order("ID2", 400);
+
+            return Ok(new List<Order> { o1, o2 });
+        }
+    }
+
+    public class Order
+    {
+        public string Id { get; set; }
+        public decimal Amount { get; set; }
+
+        public Order(string id, decimal amount)
+        {
+            Id = id;
+            Amount = amount;
+        }
+    }
+}
